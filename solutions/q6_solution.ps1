50b091975e3239521256238caf2b14ce2302fa09
Peadar Grant <peadargrant@gmail.com>
2021-10-11 12:48:05 +0100

    lab sheet solutions

---
+++ solutions/q6_solution.ps1
@@ -0,0 +1,13 @@
+﻿$T = 30
+$RH = 60
+
+$b = 18.678
+$c = 257.14
+
+$y = [math]::Log($RH/100) + $b*$T/($c+$T)
+
+Write-Host "y = $y"
+
+$Tdp = $c * $y / ( $b - $y )
+
+Write-Host "Dew point = $Tdp C"
---
+++ solutions/q8.ps1
@@ -0,0 +1,13 @@
+﻿$T = 26
+$Tdp = 24
+
+$b = 18.678
+$c = 257.14
+
+$y = $Tdp * $b / ( $c + $Tdp ) 
+
+$RH = 100 * [math]::Exp($y - $b * $T / ( $c + $T ))
+
+Write-Host "RH = $RH %"
+
+ 
